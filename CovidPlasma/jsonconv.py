f0e07147111896822cf2042fbba6b9cc6d13d395
Harsh Arora <harsh@MacBook-Air-de-Harsh.local>
2020-07-09 02:03:17 +0530

    json done

--- CovidPlasma/jsonconv.py
+++ CovidPlasma/jsonconv.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('./covidListFile.csv')
-df.set_index('id',inplace=True)
-df.to_json('./cov.json')
+df = pd.read_csv('./covidList.csv')
+#df.set_index('id',inplace=True)
+df.to_json('./cov.json',orient='records')
