8041c336abd1af2ef689d901bbc97e9cced93aae
Agam Jolly <me@agamjolly.com>
2020-07-10 04:51:16 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -37,4 +37,4 @@ def checklist():
     return render_template('test.html') 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run() # host='127.0.0.1', port=8000, debug=True)
