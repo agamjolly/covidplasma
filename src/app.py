15757c5240e7d7da547358fa3ddbc51ff9b26352
agamjolly <agamjolly@berkeley.edu>
2020-07-10 00:38:12 +0530

    about hyperlinks

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ json_url = os.path.join(SITE_ROOT, "static/data", "data.json")
 data = json.load(open(json_url))
 
 def auth(user_key=None): 
-    if user_key == "Alpha3Hotel": 
+    if user_key == "PUBLIC_KEY_HERE": 
         return True
     return False 
 
