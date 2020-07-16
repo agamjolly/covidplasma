29bbea886dae18b1c8843901acd3c9d1c9ab311d
agamjolly <agamjolly@berkeley.edu>
2020-07-16 21:47:39 +0530

    error handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request, jsonify, abort
 import json
 import os
 
@@ -13,12 +13,17 @@ def auth(user_key=None):
         return True
     return False 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/api/v1/', methods=['POST'])
 def api():
     inp = request.json
     user_key = inp['key']
     if auth(user_key): 
         return jsonify(data)
+    return jsonify(error="Invalid key"), 406
     
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -38,3 +43,4 @@ def checklist():
 
 if __name__ == '__main__':
     app.run(debug = True, port = 8000) # host='127.0.0.1', port=8000, debug=True)
+
